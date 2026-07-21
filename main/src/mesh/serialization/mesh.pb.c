7cd429b7ff2ac72776cf4658ffb77357ca0bfd55
Benjamin Swanepoel <49689582+superbrobenji@users.noreply.github.com>
2026-07-21 23:48:34 +0200

    build(deps): bump lattice-protocol to v0.4.1; regenerate nanopb codec for proto v3 fields
    Replaces the Phase-4 hand-edit of mesh.pb.h with a proper nanopb-0.4.9.1
    regen (now that mesh.options declares max_size for tags 12-16), adding
    routeLen/routePath/authTag and correcting mesh_MeshMessage_size.

--- main/src/mesh/serialization/mesh.pb.c
+++ main/src/mesh/serialization/mesh.pb.c
@@ -6,7 +6,7 @@
 #error Regenerate this file with the current version of nanopb generator.
 #endif
 
-PB_BIND(mesh_MeshMessage, mesh_MeshMessage, AUTO)
+PB_BIND(mesh_MeshMessage, mesh_MeshMessage, 2)
 
 
 
